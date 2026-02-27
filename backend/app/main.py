bd023364509637587c149d17be8853c9224427f6
jansonprotocol <dmw.boudewijn@gmail.com>
2026-02-27 11:30:56 +0100

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,6 +5,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.api.routes_health import router as health_router
 from app.api.routes_auth import router as auth_router
 from app.api.routes_league import router as league_router
+from app.api.routes_simulate import router as simulate_router
 
 # Database
 from app.database.base import Base
@@ -54,3 +55,4 @@ def startup_event():
 app.include_router(health_router, prefix="/health", tags=["Health"])
 app.include_router(auth_router, prefix="/api/auth", tags=["Auth"])
 app.include_router(league_router, prefix="/api", tags=["LeagueConfig"])
+app.include_router(simulate_router, prefix="/api", tags=["Simulate"])
