7222f600b531beaa64dc10b598b80c2fccb6de16
jansonprotocol <fragmentedmusicnl@gmail.com>
2026-03-13 11:31:54 +0100

    Update db.py

--- backend/app/database/db.py
+++ backend/app/database/db.py
@@ -2,6 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import os
 
+print(f"DEBUG: I am looking in: {os.getcwd()}")
+print(f"DEBUG: The URL I found is: {os.getenv('DATABASE_URL')}")
+
 DATABASE_URL = os.getenv("DATABASE_URL", "")
 
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
